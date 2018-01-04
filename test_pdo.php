c6b406030f673f305f86927bf71677d8f902c36e
jmz <1125378902@qq.com>
2018-01-04 18:27:30 +0800

    开发中。。。

--- test_pdo.php
+++ test_pdo.php
@@ -13,11 +13,38 @@ function dd($data, $isDump = false)
 try{
 	$pdo  = new \pdo($dsn,$user,$pwd);
 
-	foreach ($pdo->query('select * from `student`') as $v) {
-		$data[] = $v;
-	}
-	dd($data);
+	$sql = <<<EOF
+		CREATE TABLE IF NOT EXISTS user(
+		id INT UNSIGNED AUTO_INCREMENT KEY,
+		username VARCHAR(20) NOT NULL UNIQUE,
+		password CHAR(32) NOT NULL,
+		email VARCHAR(30) NOT NULL
+		);
+EOF;
+
+	$sqlA1 = "INSERT `user`(`username`,`password`,`email`) VALUES('king','".md5('king')."','112537890@qq.com')";
 	
+	$sqlB1 ="UPDATE `user` set `username` = 'jmz',`password`='".md5('jmz')."' WHERE `id` =1";
+
+	$sqlC1 ="DELETE FROM `user` WHERE `id` = 1";
+
+
+
+	$res = $pdo->exec($sql);
+	var_dump($res);
+	
+	//增
+	// $res = $pdo->exec($sqlA1);
+	
+	//改
+	// $res = $pdo->exec($sqlB1);
+	
+	//删
+	//$res = $pdo->exec($sqlC1);
+
+
+
+
 }catch(\PDOException $e){
 	print "Errot:" . $e->getMessage() . "<br/>";
 	die();
