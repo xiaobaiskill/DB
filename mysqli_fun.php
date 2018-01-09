20898a9fb90224b26daabd688247df240860e014
jmz <1125378902@qq.com>
2018-01-09 18:24:42 +0800

    添加了perse_order。perse_group查询有点问题

--- mysqli_fun.php
+++ mysqli_fun.php
@@ -1,96 +1,89 @@
 <?php
 global $comparison;
 
-$comparison = array(
-		'eq' => '=',
-		'neq'=>'!=',
-		'gt'=>'>',
-		'egt'=>'>=',
-		'lt'=>'<',
-		'elt'=>'<=',
-);
-
+$comparison = [
+	'eq'  => '=',
+	'neq' => '!=',
+	'gt'  => '>',
+	'egt' => '>=',
+	'lt'  => '<',
+	'elt' => '<='
+];
 
 function dd($data, $isDump = false)
 {
-    echo '<pre>';
-    $isDump ? var_dump($data) : print_r($data);
+	echo '<pre>';
+	$isDump ? var_dump($data) : print_r($data);
 
 }
-
 
 /**
  * where 条件拼装
  * @param  [type] $data [description]
  * @return [type]       [description]
  */
- $where1=array(
-    'b'=>'ll',
-    'c'=>array('in',[2,34,5]),
-    '_logic'=>'and'
- );
-
-$where=array(
-    'a'=>'sa',
-    '_complex'=>$where1,
-    '_logic'=>'or'
- );
-
-
-
-function perse_where ($where,$data = null){
+function perse_where($where, $data = null)
+{
 	$comparison = $GLOBALS['comparison'];
-	$where_str = '';
-	if($data === null)
-	{
-		if(is_array($where) && !empty($where)) {
-			if(array_key_exists('_logic', $where)){
+	$where_str  = '';
+	if (null === $data) {
+		if (is_array($where) && !empty($where)) {
+			if (array_key_exists('_logic', $where)) {
 				$operate = ' ' . strtoupper($where['_logic']) . ' ';
 				unset($where['_logic']);
-			}else{
+			} else {
 				$operate = ' AND ';
 			}
+			$i     = 0;
+			$count = count($where);
 			foreach ($where as $k => $v) {
 				$where_str .= ' ( ';
-				if( strpos($k, '_') !== false ){
-					$where_str = special_where($k,$v);
-				}else{
-					if(is_string($v)){
-						$where_str .= ' `'.$k.'`=\''.$v.'\' ';
-					}elseif(is_numeric($v)){
-						$where_str .= ' `'.$k.'`='.$v .' ';
-					}elseif(is_array($v)){	
-						if( array_key_exists(strtolower($v[0]), $comparison) ){
-							$where_str .=' `'.$k.'` '.$comparison[strtoloup($v[0])] .' '.(int)$v[1].' ';
-						}elseif(in_array(strtolower($v[0]), ['like','not like'])){
-							$where_str .=' `'.$k.'` '.strtoupper($v[0]) .' \''.$v[1].'\' ';
-						}elseif('in' == strtolower($v[0])){
+				if (strpos($k, '_') !== false) {
+					$where_str .= special_where($k, $v);
+				} else {
+					if (is_string($v)) {
+						$where_str .= ' `' . $k . '`=\'' . $v . '\' ';
+					} elseif (is_numeric($v)) {
+						$where_str .= ' `' . $k . '`=' . $v . ' ';
+					} elseif (is_array($v)) {
+						if (array_key_exists(strtolower($v[0]), $comparison)) {
+							$where_str .= ' `' . $k . '` ' . $comparison[strtolower($v[0])] . ' ' . (int) $v[1] . ' ';
+						} elseif (in_array(strtolower($v[0]), ['like', 'not like'])) {
+							$where_str .= ' `' . $k . '` ' . strtoupper($v[0]) . ' \'' . $v[1] . '\' ';
+						} elseif ('in' == strtolower($v[0])) {
 							$array_to_str = $v[1];
-							if(is_array($array_to_str)){
+							if (is_array($array_to_str)) {
 								$array_to_str = implode(',', $array_to_str);
 							}
-							$where_str .= ' `'.$k.'` IN('.$array_to_str.') ';
-						}elseif( 'between' == strtolower($v[0]) ){
-							$where_str .= ' `'.$k.'` BETWEEN '.str_replace(',',' AND ', $v[1]).' ';
-						}else{
-							throw new \Exception("perse_where():".$v."暂未处理", 1);	
+							$where_str .= ' `' . $k . '` IN(' . $array_to_str . ') ';
+						} elseif ('between' == strtolower($v[0])) {
+							if (is_array($v[1])) {
+								$where_str .= ' `' . $k . '` BETWEEN ' . $v[1][0] . ' AND ' . $v[1][1] . ' ';
+							} else {
+								$where_str .= ' `' . $k . '` BETWEEN ' . str_replace(',', ' AND ', $v[1]) . ' ';
+							}
+						} else {
+							throw new \Exception("perse_where():" . $v . "暂未处理", 1);
 						}
-					}else{
-						throw new \Exception("perse_where():".$v."非法条件", 1);
+					} else {
+						throw new \Exception("perse_where():" . $v . "非法条件", 1);
 					}
 				}
-				$where_str .= ' ) '.$operate;
-				dd($where_str);
+				$where_str .= ' ) ';
+				$i++;
+				if ($i !== $count) {
+					$where_str .= $operate;
+				}
 			}
 		} else {
 			throw new \Exception("parse_where():第二参数不存在，则第一参数只能是array类型", 1);
 		}
-	}else{
-		if(is_string($where)){
-			if(is_numeric($data)){
-				$where_str = '`'.$where.'` = '.$data.' ';
-			} elseif(is_string($data)){
-				$where_str = '`'.$where.'` = \''.$data.'\' ';
+	} else {
+		if (is_string($where)) {
+			if (is_numeric($data)) {
+				$where_str = '`' . $where . '` = ' . $data . ' ';
+			} elseif (is_string($data)) {
+				$where_str = '`' . $where . '` = \'' . $data . '\' ';
 			} else {
 				throw new \Exception("parse_where():第二参数类型有误", 1);
 			}
@@ -101,8 +94,7 @@ function perse_where ($where,$data = null){
 	return $where_str;
 }
 
-
-function special_where($key,$val)
+function special_where($key, $val)
 {
 	switch ($key) {
 		case '_string':
@@ -115,34 +107,54 @@ function special_where($key,$val)
 	return $where_str;
 }
 
-dd(perse_where($where));
-
-function join1()
-{
-    return;
-}
-
-function field()
+function perse_join()
 {
 	return;
 }
 
-function order()
+function perse_order($order)
 {
-	return;
+	if (!empty($order)) {
+		$order_str = '';
+		if (is_string($order)) {
+			$order_str .= ' ORDER BY ' . $order . ' ';
+		} elseif (is_array($order)) {
+			$order_str .= ' ORDER BY ';
+			$i     = 0;
+			$count = count($order);
+			foreach ($order as $k => $v) {
+				$order_str .= '`' . $k . '` ' . $v;
+				$i++;
+				if ($i !== $count) {
+					$order_str .= ', ';
+				}
+			}
+		}
+		return $order_str;
+	} else {
+		return;
+	}
 }
 
-function group()
+function perse_group($group)
 {
-	return;
+	if (!empty($group)) {
+		$group_str = '';
+		if (is_string($group)) {
+			$group_str .= 'GROUP BY ' . $group . ' ';
+		}
+		return $group_str;
+	} else {
+		return;
+	}
 }
-function limit()
+function perse_limit()
 {
 	return;
 }
 
 // 初始化操作
-/*
+
 $host    = '127.0.0.1';
 $user    = 'vagrant';
 $pwd     = 'vagrant';
@@ -154,4 +166,27 @@ $db = new \mysqli($host, $user, $pwd, $name, $port);
 
 $db->select_db($name);      //切换数据库
 $db->set_charset($charset); //编码
-*/
+
+$where1 = [
+	'class'  => ['gt', 2],
+	'status' => 1
+];
+
+$where = [
+	'id'       => ['in', [1, 2, 10, 40, 27, 37, 12, 8]],
+	'_complex' => $where1,
+	'_logic'   => 'or'
+];
+
+$sql  = 'select name,sum(class),status from `student` where ' . perse_where($where) . ' group by class ' . perse_order(['class' => 'asc', 'id' => 'desc']);
+echo $sql;
+echo '<hr>';
+$sqlD6 = "select a.`sid`,b.`name`,sum(a.`score`) as `ascore`,count(a.`id`) as `cid`  from `score` a left join `student` b on a.`sid` =  b.`id` left join `subject` c on c.`id` = a.`subject_id` where a.`id` < 20 group by a.`sid` having avg(a.`score`) > 70 order by a.`sid` desc limit 0,2";
+echo $sqlD6;
+echo '<hr>';
+$sqlD5 = "select `sid`,avg(`score`) as `score` from `score` where id >3 group by `sid`";
+echo $sqlD5;
+echo '<hr>';
+$stmt = $db->query($sql);
+dd($stmt->fetch_all(MYSQLI_ASSOC));
+$db->close();
